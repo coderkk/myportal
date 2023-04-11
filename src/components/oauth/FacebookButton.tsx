dd107acf4762418a61b9142e303b516ac00578c7
Chan Che Wei <chanchewei@moneymatch.co>
2023-04-11 23:11:14 +0800

    Change the callbackUrl to /projects

--- src/components/oauth/FacebookButton.tsx
+++ src/components/oauth/FacebookButton.tsx
@@ -6,7 +6,7 @@ const FacebookButton = () => {
     <button
       className="relative inline-flex items-center justify-center rounded-md border border-gray-700 bg-gray-800 px-6 py-3 text-lg font-medium text-white shadow-sm hover:bg-gray-700 hover:text-gray-100"
       type="button"
-      onClick={() => void signIn("facebook", { callbackUrl: "/" })}
+      onClick={() => void signIn("facebook", { callbackUrl: "/projects" })}
     >
       <span className="flex items-center">
         <span className="sr-only">Sign in with</span>
