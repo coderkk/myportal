09f7d418a887786f3508c4c96653f3b4fa8813e9
Calvin Tai <calvintai0402@gmail.com>
2023-04-11 01:02:07 -0700

    try python api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,10 @@ import { Hero } from "../components/landing/Hero";
 import { Pricing } from "../components/landing/Pricing";
 import { PrimaryFeatures } from "../components/landing/PrimaryFeatures";
 import { SecondaryFeatures } from "../components/landing/SecondaryFeatures";
-import { api } from "../utils/api";
 
 const Home: NextPage = () => {
-  const { data } = api.python.test.useQuery();
-  console.log(data);
+  // const { data } = api.python.test.useQuery();
+  // console.log(data);
   return (
     <>
       <Head>
