37b705602929522c4734176f6c8138b0f76c1a84
Calvin Tai <calvintai0402@gmail.com>
2023-04-11 01:28:26 -0700

    test

--- api/testing.py
+++ api/testing.py
@@ -1,13 +1,10 @@
-# print("hello world")
-
 from http.server import BaseHTTPRequestHandler
 
 
 class handler(BaseHTTPRequestHandler):
- 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
+        self.wfile.write(str('Hello World!!').encode())
         return
