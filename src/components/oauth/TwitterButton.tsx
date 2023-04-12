54055dc12024dd88ba79855e7dd44237c5fb5360
Chan Che Wei <chanchewei@moneymatch.co>
2023-04-12 21:36:13 +0800

    Added signIn function for the TwitterButton

--- src/components/oauth/TwitterButton.tsx
+++ src/components/oauth/TwitterButton.tsx
@@ -1,10 +1,12 @@
 import { Twitter } from "@styled-icons/bootstrap/Twitter";
+import { signIn } from "next-auth/react";
 
 const TwitterButton = () => {
   return (
     <button
       className="relative inline-flex items-center justify-center rounded-md border border-gray-700 bg-gray-800 px-6 py-3 text-lg font-medium text-white shadow-sm hover:bg-gray-700 hover:text-gray-100"
       type="button"
+      onClick={() => void signIn("twitter", { callbackUrl: "/projects" })}
     >
       <span className="flex items-center">
         <span className="sr-only">Sign in with</span>
