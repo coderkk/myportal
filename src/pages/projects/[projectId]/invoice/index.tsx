367ef0bd54fe7778a9f734c01c47d1072ff71473
YH Lai <yhlaikk@gmail.com>
2023-04-15 23:32:01 +0800

    parseInvoice

--- src/pages/projects/[projectId]/invoice/index.tsx
+++ src/pages/projects/[projectId]/invoice/index.tsx
@@ -1,7 +1,12 @@
-import { useRouter } from "next/router";
-import { useRef, useState } from "react";
 import SessionAuth from "../../../../components/auth/SessionAuth";
 import { Document, Page, pdfjs } from "react-pdf";
+import { useRef, useState } from "react";
+import type { PDFDocumentProxy } from "pdfjs-dist";
+import { getPDFText, parseData } from '../../../../utils/pdfparser';
+// import { useRouter } from "next/router";
+// import { useRef, useState } from "react";
+
+import type { InvoiceType } from '../../../../utils/pdfparser';
 
 pdfjs.GlobalWorkerOptions.workerSrc = '/js/pdf.worker.min.js';
 // pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
@@ -10,14 +15,46 @@ const Invoice = () => {
   // const router = useRouter();
   // const projectId = router.query.projectId as string;
   // const pendingDeleteCountRef = useRef(0); // prevent parallel GET requests as much as possible. # https://profy.dev/article/react-query-usemutation#edge-case-concurrent-updates-to-the-cache
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const [file, setFile] = useState<string>('/docs/invoice.pdf');
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+  const [invoiceData, setInvoiceData] = useState<InvoiceType>({
+    vendorName: "ds",
+    invoiceNo: "",
+    invoiceDate: "",
+    invoiceCosts: 0,
+    description: "",
+  });
+  const [uploadFile, setUploadFile] = useState<File | undefined>(undefined);
+  const [file, ] = useState<string>('/docs/invoice.pdf');
+  const [numPages, setNumPages] = useState<number | null>(null);
+  const [pageNumber, ] = useState<number>(1);
   
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
+  function onDocumentLoadSuccess(pdf: PDFDocumentProxy ) {
+    setNumPages(pdf.numPages);
+    parseInvoice(pdf);
   }
+
+  async function parseInvoice(pdf: PDFDocumentProxy) {
+    await getPDFText(pdf).then((pdfText) => {
+        const data = parseData(pdfText)
+        setInvoiceData(data);
+    })
+  }
+
+  const handleUploadClick = () => {
+    // 👇 We redirect the click event onto the hidden input element
+    inputRef.current?.click();
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+
+    setUploadFile(e.target.files[0]);
+
+    // 🚩 do the file upload here normally...
+  };
 
   return (
     <SessionAuth>
@@ -25,11 +62,24 @@ const Invoice = () => {
             <div className="m-auto">
                 <section>
                     <div className="max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+                        <div className="flex justify-between mb-5">
+                            <h2 className="text-3xl font-bold sm:text-4xl">Invoice Data Extraction</h2>
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleUploadClick}>
+                                {uploadFile ? `${uploadFile.name}` : 'Click to select'}
+                            </button>
+
+                            {/* 👇 Notice the `display: hidden` on the input */}
+                            <input
+                                type="file"
+                                ref={inputRef}
+                                onChange={handleFileChange}
+                                style={{ display: 'none' }}
+                            />
+                        </div>
                         <div
                             className="grid grid-cols-1 gap-y-8 lg:grid-cols-2  lg:gap-x-16"
                         >
                             <div className="mx-auto max-w-lg text-left lg:mx-0 lg:text-left">
-                                <h2 className="text-3xl font-bold sm:text-4xl">Invoice Data Extraction</h2>
 
                                 <div className="overflow-hidden bg-white shadow sm:rounded-lg">
                                     <div className="px-4 py-5 sm:px-6">
@@ -40,23 +90,23 @@ const Invoice = () => {
                                         <dl className="sm:divide-y sm:divide-gray-200">
                                             <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 sm:py-5">
                                                 <dt className="text-sm font-medium text-gray-500">Vendor name</dt>
-                                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Margot Foster</dd>
+                                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{invoiceData.vendorName}</dd>
                                             </div>
                                             <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 sm:py-5">
                                                 <dt className="text-sm font-medium text-gray-500">Invoice No</dt>
-                                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Backend Developer</dd>
+                                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{invoiceData.invoiceNo}</dd>
                                             </div>
                                             <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 sm:py-5">
                                                 <dt className="text-sm font-medium text-gray-500">Invoice Date</dt>
-                                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">margotfoster@example.com</dd>
+                                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{invoiceData.invoiceDate}</dd>
                                             </div>
                                             <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 sm:py-5">
                                                 <dt className="text-sm font-medium text-gray-500">Invoice Costs</dt>
-                                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">$120,000</dd>
+                                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{invoiceData.invoiceCosts}</dd>
                                             </div>
                                             <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 sm:py-5">
                                                 <dt className="text-sm font-medium text-gray-500">Invoice Description</dt>
-                                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.</dd>
+                                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{invoiceData.description}</dd>
                                             </div>
                                             <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 sm:py-5">
                                                 <dt className="text-sm font-medium text-gray-500">Assign to</dt>
@@ -72,13 +122,13 @@ const Invoice = () => {
 
                             </div>
 
-                            <div className="grid grid-cols-2 gap-4 sm:grid-cols-3">
-                                <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
-                                    <Page pageNumber={pageNumber} renderTextLayer={false} />
-                                </Document>
-                                <p>
-                                    Page {pageNumber} of {numPages}
-                                </p>
+                            <div className="border-solid border-2 border-sky-500 clear-both">
+                                    <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+                                        <Page pageNumber={pageNumber} renderTextLayer={false} renderAnnotationLayer={false}  />
+                                    </Document>
+                                    <p>
+                                        Page {pageNumber} of {numPages}
+                                    </p>
                             </div>
                         </div>
                     </div>
---
+++ src/utils/pdfparser.ts
@@ -0,0 +1,90 @@
+import type { PDFDocumentProxy } from "pdfjs-dist";
+import * as pdfjsLib from "pdfjs-dist";
+import { GlobalWorkerOptions } from "pdfjs-dist";
+import type {
+  DocumentInitParameters,
+  TextItem,
+  TextMarkedContent,
+  TypedArray,
+} from "pdfjs-dist/types/src/display/api";
+import { useState } from "react";
+
+export interface InvoiceType {
+    vendorName: string;
+    invoiceNo: string | undefined;
+    invoiceDate: string | undefined;
+    invoiceCosts: number | undefined;
+    description: string;
+}
+
+GlobalWorkerOptions.workerSrc = "/js/pdf.worker.min.js";
+
+const getPageText = async (pdf: PDFDocumentProxy, pageNumber: number) => {
+  const page = await pdf.getPage(pageNumber);
+  const pageText = await page.getTextContent();
+  return pageText.items
+    .filter((token: TextItem | TextMarkedContent) => {
+      return "hasEOL" in token;
+    })
+    .map((token: TextItem | TextMarkedContent) => {
+      if ("hasEOL" in token) {
+        return token.str.toLowerCase() + (token.hasEOL ? "\n" : "");
+      }
+    })
+    .join("");
+};
+
+export const getPDFText = async (
+  pdf: PDFDocumentProxy
+): Promise<string> => {
+  const pageTextPromises = [];
+  for (let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber += 1) {
+    pageTextPromises.push(getPageText(pdf, pageNumber));
+  }
+  const pageTexts = await Promise.all(pageTextPromises);
+  return pageTexts.join("\n");
+};
+
+export const loadPDF = async (
+    source: string | URL | TypedArray | ArrayBuffer | DocumentInitParameters
+  ): Promise<string> => {
+    const loadingTask = pdfjsLib.getDocument(source);
+    const pdf = await loadingTask.promise;
+    return await getPDFText(pdf)
+  };
+
+export const parseData = (pdfContent: string) : InvoiceType => {
+    const data: InvoiceType =  {
+        vendorName: "ds",
+        invoiceNo: "",
+        invoiceDate: "",
+        invoiceCosts: 0,
+        description: "",
+    }
+    const pageTexts: string[] = pdfContent.split("\n")
+    for (const pageText of pageTexts) {
+        const pageTextLines = pageText.split(/\r?\n/);
+        pageTextLines.forEach((pageTextLine) => {
+          if (pageTextLine.includes("invoice number")) {
+            data.invoiceNo = pageTextLine.match(/\d/g)?.join("");
+          }
+          if (pageTextLine.includes("invoice date")) {
+            if (pageTextLine.match(/\d{2}\/\d{2}\/\d{4}/))
+            data.invoiceDate = pageTextLine.match(/\d{2}\/\d{2}\/\d{4}/)?.join("");
+            if (pageTextLine.match(/\d{2}-\d{2}-\d{4}/))
+            data.invoiceDate = pageTextLine.match(/\d{2}-\d{2}-\d{4}/)?.join("");
+          }
+          if (
+            pageTextLine.includes("total") &&
+            pageTextLine.split(" ").includes("total")
+          ) {
+            if (pageTextLine.match(/\d/g)) {
+                const val = pageTextLine.match(/\d+(?:\.\d{2})?/)?.join("")
+                data.invoiceCosts = (val == undefined) ? 0 : parseFloat(val);
+            }
+          }
+        });
+      }
+
+    return data;
+}
