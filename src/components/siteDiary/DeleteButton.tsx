02011b8377933a47ea0d81e89268312cbf9a199f
MikeIrlbeck <91171891+MikeIrlbeck@users.noreply.github.com>
2023-04-22 11:57:08 -0600

    styled site diary delete modal

--- src/components/siteDiary/DeleteButton.tsx
+++ src/components/siteDiary/DeleteButton.tsx
@@ -26,18 +26,42 @@ const DeleteButton = ({
         <Delete className="h-6 w-6 text-red-500" />
       </AlertDialog.Trigger>
       <AlertDialog.Portal>
-        <AlertDialog.Overlay className="fixed inset-0 animate-fade-in bg-slate-300" />
-        <AlertDialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-content-show rounded-md bg-white p-6 shadow-md focus:outline-none">
-          <AlertDialog.Title className="m-0 font-medium text-gray-800">
-            Are you absolutely sure?
+        <AlertDialog.Overlay className="fixed inset-0 animate-fade-in bg-gray-500 bg-opacity-75 transition-opacity" />
+        <AlertDialog.Content className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+          <AlertDialog.Title
+            className="mt-3 mb-5  flex flex-col gap-3 text-center text-lg font-bold leading-6 text-gray-900 sm:mt-5 sm:flex-row sm:items-start sm:text-left"
+            aria-describedby="This action cannot be undone."
+          >
+            <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10 ">
+              <svg
+                class="h-6 w-6 text-red-600"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                />
+              </svg>
+            </div>
+            <div className="flex flex-col gap-1">
+              <p className="capitalize">confirm delete</p>
+              <p className="mx-0 text-sm font-medium text-gray-500">
+                This action cannot be undone.
+              </p>
+            </div>
           </AlertDialog.Title>
-          <AlertDialog.Description className="mx-0 mt-3 mb-5 text-sm text-gray-400">
-            This action cannot be undone. This will permanently delete your site
-            diary and remove your data from our servers.
-          </AlertDialog.Description>
-          <div className="flex justify-end gap-6">
+          <div className="mt-5 flex flex-col-reverse sm:flex-row sm:gap-3">
             <AlertDialog.Cancel asChild>
-              <button className="inline-flex h-9 items-center justify-center rounded-md bg-gray-100 py-0 px-4 text-sm font-medium text-gray-700 hover:bg-gray-200">
+              <button
+                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                aria-label="Close"
+                type="button"
+              >
                 Cancel
               </button>
             </AlertDialog.Cancel>
@@ -52,9 +76,9 @@ const DeleteButton = ({
                     router.back();
                   }
                 }}
-                className="inline-flex h-9 items-center justify-center rounded-md bg-red-100 py-0 px-4 text-sm font-medium text-red-700 hover:bg-red-200"
+                className="inline-flex w-full justify-center rounded-md  bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm  hover:bg-red-500  sm:col-start-2"
               >
-                Yes, delete site diary
+                Delete
               </button>
             </AlertDialog.Action>
           </div>
