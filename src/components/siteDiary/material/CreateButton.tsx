43738cfb8fdaeeeada945fdcb63da2f933b5d15e
MikeIrlbeck <91171891+MikeIrlbeck@users.noreply.github.com>
2023-04-22 11:02:54 -0600

    accidentally deleted from 'replace all'

--- src/components/siteDiary/material/CreateButton.tsx
+++ src/components/siteDiary/material/CreateButton.tsx
@@ -127,7 +127,7 @@ const CreateButton = ({ siteDiaryId }: { siteDiaryId: string }) => {
               </button>
               <Dialog.Close asChild>
                 <button
-                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0"
+                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
                   aria-label="Close"
                   type="button"
                 >
