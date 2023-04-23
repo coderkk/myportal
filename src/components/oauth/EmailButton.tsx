abf825f1dc58c1be5c91559b07e70a9f8780cde3
Calvin Tai <calvintai0402@gmail.com>
2023-04-22 22:45:38 -0700

    disable submit button if email erros out

--- src/components/oauth/EmailButton.tsx
+++ src/components/oauth/EmailButton.tsx
@@ -60,6 +60,7 @@ const EmailButton = () => {
         className="relative mt-2 inline-flex items-center justify-center rounded-md border border-gray-700 bg-gray-800 px-6 py-3 text-lg font-medium text-white shadow-sm hover:bg-gray-700 hover:text-gray-100"
         data-te-ripple-init
         data-te-ripple-color="light"
+        disabled={!!errors.email}
       >
         <span className="flex items-center">
           <span className="sr-only">Sign in with</span>
