b3cd56768bbaa51473d35d9896bb2079b461c9ee
Calvin Tai <calvintai0402@gmail.com>
2023-04-26 20:24:37 -0700

    added weather

--- src/pages/projects/[projectId]/site-diary/[siteDiaryId].tsx
+++ src/pages/projects/[projectId]/site-diary/[siteDiaryId].tsx
@@ -133,132 +133,6 @@ const SiteDiary = () => {
           <div>Loading...</div>
         ) : (
           siteDiary && (
-            // <div className="flex h-[80vh]">
-            //   <div className="m-auto w-10/12">
-            //     <div className="text-lg font-medium">{siteDiary.name}</div>
-            //     <div className="flex justify-between">
-            //       <div>Weather condition</div>
-            //       <div>
-            //         <span className="mr-2">
-            //           Morning:{" "}
-            //           <Dropdown
-            //             weatherCondition={siteDiary.weather?.morning}
-            //             onWeatherChange={(value: WeatherCondition) =>
-            //               onWeatherChange({
-            //                 weather: siteDiary.weather,
-            //                 time: "M",
-            //                 value: value,
-            //               })
-            //             }
-            //           />
-            //         </span>
-            //         <span className="mr-2">
-            //           Afternoon:{" "}
-            //           <Dropdown
-            //             weatherCondition={siteDiary.weather?.afternoon}
-            //             onWeatherChange={(value: WeatherCondition) =>
-            //               onWeatherChange({
-            //                 weather: siteDiary.weather,
-            //                 time: "A",
-            //                 value: value,
-            //               })
-            //             }
-            //           />
-            //         </span>
-            //         Evening:
-            //         <Dropdown
-            //           weatherCondition={siteDiary.weather?.evening}
-            //           onWeatherChange={(value: WeatherCondition) =>
-            //             onWeatherChange({
-            //               weather: siteDiary.weather,
-            //               time: "E",
-            //               value: value,
-            //             })
-            //           }
-            //         />
-            //       </div>
-            //     </div>
-            //     <div className="mt-2 flex justify-center">
-            //       <Tabs.Root
-            //         className="flex w-2/3 flex-col  shadow"
-            //         defaultValue="tab1"
-            //       >
-            //         <Tabs.List
-            //           className="flex shrink-0 border-b-2 border-solid border-gray-200"
-            //           aria-label="Manage your account"
-            //         >
-            //           <Tabs.Trigger
-            //             className="flex flex-1 select-none items-center justify-center rounded-tl-md bg-white px-5 text-base
-            //       text-blue-200 hover:text-blue-500 data-[state=active]:text-blue-500 data-[state=active]:shadow-inner"
-            //             value="tab1"
-            //           >
-            //             Plants
-            //           </Tabs.Trigger>
-            //           <Tabs.Trigger
-            //             className="flex flex-1 select-none items-center justify-center bg-white px-5 text-base text-blue-200
-            //       hover:text-blue-500 data-[state=active]:text-blue-500 data-[state=active]:shadow-inner"
-            //             value="tab2"
-            //           >
-            //             Laborers
-            //           </Tabs.Trigger>
-            //           <Tabs.Trigger
-            //             className="flex flex-1 select-none items-center justify-center bg-white px-5 text-base text-blue-200
-            //       hover:text-blue-500 data-[state=active]:text-blue-500 data-[state=active]:shadow-inner"
-            //             value="tab3"
-            //           >
-            //             Materials
-            //           </Tabs.Trigger>
-            //           <Tabs.Trigger
-            //             className="flex flex-1 select-none items-center justify-center bg-white px-5 text-base text-blue-200
-            //       hover:text-blue-500 data-[state=active]:text-blue-500 data-[state=active]:shadow-inner"
-            //             value="tab4"
-            //           >
-            //             Site Problems
-            //           </Tabs.Trigger>
-            //           <Tabs.Trigger
-            //             className="flex flex-1 select-none items-center justify-center rounded-tr-md bg-white px-5 text-base
-            //       text-blue-200 hover:text-blue-500 data-[state=active]:text-blue-500 data-[state=active]:shadow-inner"
-            //             value="tab5"
-            //           >
-            //             Work progress
-            //           </Tabs.Trigger>
-            //         </Tabs.List>
-            //         <Tabs.Content
-            //           className="flex-grow rounded-bl-md rounded-br-md bg-white p-5 focus:shadow"
-            //           value="tab1"
-            //         >
-            //           <PlantView plants={siteDiary.plants} />
-            //         </Tabs.Content>
-            //         <Tabs.Content
-            //           className="flex-grow rounded-bl-md rounded-br-md bg-white p-5  focus:shadow"
-            //           value="tab2"
-            //         >
-            //           <LaborerView laborers={siteDiary.laborers} />
-            //         </Tabs.Content>
-            //         <Tabs.Content
-            //           className="flex-grow rounded-bl-md rounded-br-md bg-white p-5  focus:shadow"
-            //           value="tab3"
-            //         >
-            //           <MaterialView materials={siteDiary.materials} />
-            //         </Tabs.Content>
-            //         <Tabs.Content
-            //           className="flex-grow rounded-bl-md rounded-br-md bg-white p-5 focus:shadow"
-            //           value="tab4"
-            //         >
-            //           <SiteProblemView siteProblems={siteDiary.siteProblems} />
-            //         </Tabs.Content>
-            //         <Tabs.Content
-            //           className="flex-grow rounded-bl-md rounded-br-md bg-white p-5 focus:shadow"
-            //           value="tab5"
-            //         >
-            //           <WorkProgressView
-            //             workProgresses={siteDiary.workProgresses}
-            //           />
-            //         </Tabs.Content>
-            //       </Tabs.Root>
-            //     </div>
-            //   </div>
-            // </div>
             <main>
               <h1 className="m-4">
                 <span className="font-semibold text-black">
@@ -322,7 +196,48 @@ const SiteDiary = () => {
                       ))}
                     </ul>
                   </Transition>
-                  <div className="mr-4 flex items-center">
+                  <div className="mr-4 flex items-center text-xs text-gray-400">
+                    <div className="mr-2 flex">
+                      <span className="mr-2 flex flex-col">
+                        Morning:{" "}
+                        <Dropdown
+                          weatherCondition={siteDiary.weather?.morning}
+                          onWeatherChange={(value: WeatherCondition) =>
+                            onWeatherChange({
+                              weather: siteDiary.weather,
+                              time: "M",
+                              value: value,
+                            })
+                          }
+                        />
+                      </span>
+                      <span className="mr-2 flex flex-col">
+                        Afternoon:{" "}
+                        <Dropdown
+                          weatherCondition={siteDiary.weather?.afternoon}
+                          onWeatherChange={(value: WeatherCondition) =>
+                            onWeatherChange({
+                              weather: siteDiary.weather,
+                              time: "A",
+                              value: value,
+                            })
+                          }
+                        />
+                      </span>
+                      <span className="mr-2 flex flex-col">
+                        Evening:
+                        <Dropdown
+                          weatherCondition={siteDiary.weather?.evening}
+                          onWeatherChange={(value: WeatherCondition) =>
+                            onWeatherChange({
+                              weather: siteDiary.weather,
+                              time: "E",
+                              value: value,
+                            })
+                          }
+                        />
+                      </span>
+                    </div>
                     {getCreateButton(currentTab)}
                   </div>
                 </nav>
