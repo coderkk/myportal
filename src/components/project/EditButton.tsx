5e083ba22a915cfe8b1e4224d545d60ad2a8c20e
MikeIrlbeck <mirlbeck22@gmail.com>
2023-05-08 06:36:03 -0600

    styled edit modal; added aria-describedby for accessibility

--- src/components/project/EditButton.tsx
+++ src/components/project/EditButton.tsx
@@ -1,6 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { Edit } from "@styled-icons/boxicons-solid/";
-import { Close } from "@styled-icons/ionicons-outline";
 import { useState, type BaseSyntheticEvent } from "react";
 import { useForm } from "react-hook-form";
 import { useUpdateProject } from "../../hooks/project";
@@ -46,32 +45,23 @@ const EditButton = ({ project }: { project: project }) => {
         <Edit className="h-6 w-6  text-green-500" />
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className="fixed inset-0 animate-fade-in bg-slate-300" />
-        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-content-show rounded-md bg-white p-6 shadow-md focus:outline-none">
-          <Dialog.Title className="m-0 font-medium text-gray-800">
-            Edit project
+        <Dialog.Overlay className="fixed inset-0 animate-fade-in bg-gray-500 bg-opacity-75 transition-opacity" />
+        <Dialog.Content
+          className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+          aria-describedby="Edit the name of an existing project"
+        >
+          <Dialog.Title className="mt-3 text-left text-lg font-bold capitalize leading-6 text-gray-900 sm:mt-5">
+            edit project
           </Dialog.Title>
-          <Dialog.Description className="mx-0 mb-5 mt-3 text-sm text-gray-400">
-            Edit your project here. Click save when you are done.
-          </Dialog.Description>
           <form onSubmit={(e) => void handleSubmit(onSubmit)(e)}>
             <fieldset className="mb-4 flex items-center gap-5">
-              <label
-                className="w-24 text-right text-sm text-blue-300"
-                htmlFor="name"
-              >
-                Name
-              </label>
-              <div>
+              <div className="sm:flex sm:flex-1 sm:flex-row sm:gap-2">
                 <input
-                  className={`inline-flex h-8  flex-1 items-center justify-center rounded-md px-3 py-0 text-sm text-blue-500 shadow-sm shadow-blue-200 focus:border-2
-                focus:border-blue-300  focus:outline-none ${
-                  errors.name
-                    ? "border-2 border-red-400 focus:border-2 focus:border-red-400"
-                    : ""
-                }`}
+                  className={`mb-3 mt-5 h-10 w-full rounded-lg border border-gray-300 px-4 py-2 text-center focus:border-blue-300 focus:outline-none sm:col-start-1 sm:text-left ${
+                    errors.name ? "border-red-400  focus:border-red-400 " : ""
+                  }`}
                   id="name"
-                  defaultValue={project.name}
+                  placeholder="Project name"
                   {...register("name", { required: true })}
                 />
               </div>
@@ -81,24 +71,24 @@ const EditButton = ({ project }: { project: project }) => {
                 Name is required
               </span>
             )}
-            <div className="mt-6 flex justify-end">
+            <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
               <button
-                className="inline-flex h-9 items-center justify-center rounded-md bg-blue-100 px-4 py-0 text-sm font-medium text-blue-700 hover:bg-blue-200 disabled:bg-blue-50 disabled:text-blue-200"
+                className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 disabled:bg-blue-50 disabled:text-blue-200 sm:col-start-2"
                 type="submit"
                 disabled={!!errors.name}
               >
-                Submit
+                Edit
               </button>
+              <Dialog.Close asChild>
+                <button
+                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                  aria-label="Close"
+                  type="button"
+                >
+                  Cancel
+                </button>
+              </Dialog.Close>
             </div>
-            <Dialog.Close asChild>
-              <button
-                className="absolute right-4 top-4 inline-flex h-6 w-6 items-center justify-center rounded-full hover:bg-blue-200 focus:border-2 focus:border-blue-500 focus:outline-none"
-                aria-label="Close"
-                type="button"
-              >
-                <Close className="h-4 w-4" />
-              </button>
-            </Dialog.Close>
           </form>
         </Dialog.Content>
       </Dialog.Portal>
