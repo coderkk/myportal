b27661fe7b0890ee45fa9f68dde3fb32b9ce5ec7
MikeIrlbeck <mirlbeck22@gmail.com>
2023-05-10 07:14:41 -0600

    styled invite btn

--- src/components/team/InviteButton.tsx
+++ src/components/team/InviteButton.tsx
@@ -18,7 +18,7 @@ const InviteButton = ({
       <AlertDialog.Trigger asChild>
         <button
           type="button"
-          className="ml-2 flex items-center rounded-lg bg-blue-500 p-2 text-white disabled:bg-blue-200"
+          className="inline-flex justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 disabled:bg-blue-200 disabled:text-blue-50 sm:col-start-2"
           disabled={usersToBeAdded.length === 0}
         >
           Add
