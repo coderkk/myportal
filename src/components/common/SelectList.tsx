aef48af129259d3cc7e687a55300deb75595a868
YH Lai <yhlaikk@gmail.com>
2023-06-10 01:14:29 +0800

    fix SelectList option menu overlay issue

--- src/components/common/SelectList.tsx
+++ src/components/common/SelectList.tsx
@@ -73,7 +73,7 @@ export default function SelectList({
         >
           <Combobox.Options
             className={classNames(
-              "absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm",
+              "absolute z-20 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm",
               filteredOptions.length === 0 && query === "" ? "hidden" : ""
             )}
           >
