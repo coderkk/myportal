e2ccb4268ebbb2504963d4825c0b4f0baff550fc
MikeIrlbeck <mirlbeck22@gmail.com>
2023-06-08 18:54:01 -0600

    improved error message for invalid type

--- src/schema/budget.tsx
+++ src/schema/budget.tsx
@@ -2,8 +2,14 @@ import { z } from "zod";
 
 export const createBudgetSchema = z.object({
   description: z.string().min(1, "A description is required"),
-  expectedBudget: z.number().positive({ message: "Budget must be positive" }),
+  expectedBudget: z
+    .number({
+      invalid_type_error: "Budget must be a number",
+    })
+    .positive({ message: "Budget must be positive" }),
   costsIncurred: z
-    .number()
+    .number({
+      invalid_type_error: "Budget must be a number",
+    })
     .positive({ message: "Costs incurred must be positive" }),
 });
