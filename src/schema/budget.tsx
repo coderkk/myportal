7f0834522f672285b0f313b1c2fd6028fb1d2f80
MikeIrlbeck <mirlbeck22@gmail.com>
2023-06-10 15:32:38 -0600

    not allowing a description that is just whitespace

--- src/schema/budget.tsx
+++ src/schema/budget.tsx
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const createBudgetSchema = z.object({
-  description: z.string().min(1, "A description is required"),
+  description: z.string().trim().min(1, "A description is required"),
   expectedBudget: z
     .number({ invalid_type_error: "Budget must be a number" })
     .positive({ message: "Budget must be positive" }),
